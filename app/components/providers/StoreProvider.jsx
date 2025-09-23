d3259add8c7c08e084978a538debe4419f008910
mighty1 <ihanashi@gmail.com>
2025-09-23 21:20:15 +0900

    22. Load balances

--- app/components/providers/StoreProvider.jsx
+++ app/components/providers/StoreProvider.jsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { useRef } from 'react'
-import { Provider } from 'react-redux'
-import { makeStore } from '@/lib/store'
+import { useRef } from "react"
+import { Provider } from "react-redux"
+import { makeStore } from "@/lib/store"
 
 export default function StoreProvider({ children }) {
   const storeRef = useRef(null)
+
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore()
