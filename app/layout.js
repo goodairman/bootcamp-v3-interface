6b98be55a6fb18f8aa944479db8f0e26e1a833ed
mighty1 <ihanashi@gmail.com>
2025-09-25 16:52:12 +0900

    23. Deposits & Withdrawals

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,34 @@
-import "./globals.css"
+// CSS
+import "./globals.css";
 
 // Fonts
-import { Lexend } from "next/font/google"
-const lexend = Lexend({ subsets: ['latin']})
+import { Lexend } from 'next/font/google'
+const lexend = Lexend({ subsets: ['latin'] })
 
 // Components
 import MetaMaskProvider from "./components/providers/MetaMaskProvider"
 import StoreProvider from "./components/providers/StoreProvider"
-import TopNav from "./components/TopNav"
-import SideNav from "./components/SideNav";
+import TopNav from './components/TopNav'
+import SideNav from './components/SideNav'
 
 export const metadata = {
   title: "DAPP Exchange",
-  description: "Your favorite peer-to-peer exchange",
+  description: "Your favorite Peer to peer orderbook exchange",
 };
 
 export default function RootLayout({ children }) {
   return (
     <StoreProvider>
-      <MetaMaskProvider>
+      <MetaMaskProvider>    
         <html lang="en">
-        <body className={`${lexend.className}`}>
-        <SideNav />
-        <main className="content">
-          <TopNav />
-          {children}
-        </main>
-        </body>
+          <body className={`${lexend.className}`}>
+            <SideNav/>
+
+            <main className="content">
+              <TopNav />
+              {children}
+            </main>
+          </body>
         </html>
       </MetaMaskProvider>
     </StoreProvider>
